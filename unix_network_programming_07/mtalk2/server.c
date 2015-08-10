44564a3eea08436981b3e14aa5e9df6bc14b1f39
Koji Ota <afterjnih@gmail.com>
2015-08-11 08:29:53 +0900

    add server.c at chapter 7

---
+++ unix_network_programming_07/mtalk2/server.c
@@ -0,0 +1,25 @@
+//
+//  server.c
+//  unix_network_program
+//
+//  Created by 太田浩二 on 2015/08/11.
+//
+//
+
+
+
+#include <stdio.h>
+#include "mtalk.h"
+#include "sessionman.h"
+
+main()
+{
+	
+	if(sessionman_init(PORT) == -1){
+		fprintf(stderr, "cannot setup server \n");
+		exit(1);
+	}
+	
+	
+	sessionman_loop();
+}
