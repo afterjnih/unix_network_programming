469d56e9c42cfeb7934e2be8fe443690a1058748
Koji Ota <afterjnih@gmail.com>
2015-09-04 23:48:27 +0900

    add service.c

---
+++ unix_network_programming_08/daemon/service.c
@@ -0,0 +1,72 @@
+//
+//  service.c
+//  unix_network_program
+//
+//  Created by 太田浩二 on 2015/09/04.
+//
+//
+
+
+#include <fcntl.h>
+#include "bb.h"
+
+#define DATA_DIR "/mnt/fat32/work/socket/src/daemon/data"
+#define MENUSTRING "1.呼び出し 2.休講 3.サークル 4.行事 9.終了 \n"
+
+
+static int process(int soc, int item)
+
+
+void service(int soc, char *myname)
+
+
+{
+	pid_t mypid;
+	char buf[BUF_LNEGTH];
+	int item;
+	
+	mypid= (int)getpid();
+	sprintf(buf, "This is %s[%d] serving you. \n",
+					myname, (int)mypid);
+	write(soc, buf, strlen(buf));
+	
+	do{
+		write(soc, MENUSTRING, strlen(MENUSTRING));
+		read(soc, buf, 20);
+		sscan(buf, "%d", &item);
+	}
+	while(process(soc, item));
+	
+	close(soc);
+}
+
+
+static int process(int soc, int item)
+
+
+{
+	int fd;
+	int cnt;
+	char filename[FILENSME_LENGTH];
+	char buf[BUF_LENGTH];
+	
+	if(item == 9)
+		return 0;
+	
+	
+	sprintf(filename, "%s/data%d", DATA_DIR, item);
+	
+	
+	if ((fd = open(filename, O_RDONLY)) == -1) {
+		write(soc, "invalid number.\n", 17);
+		return 1;
+	}
+	
+	
+	while((cnt=read(fd, buf, BUF_LENGTH)) != 0)
+		write(soc, buf, cnt);
+	
+	
+	close(fd);
+	return 1;
+}
