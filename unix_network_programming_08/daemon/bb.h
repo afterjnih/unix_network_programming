07b6281e307232676403a4cb9278b86da445a69e
Koji Ota <afterjnih@gmail.com>
2015-09-04 23:51:48 +0900

    add bb.h

---
+++ unix_network_programming_08/daemon/bb.h
@@ -0,0 +1,21 @@
+//
+//  bb.h
+//  unix_network_program
+//
+//  Created by 太田浩二 on 2015/09/04.
+//
+//
+#include <stdio.h>
+#include <netinet/in.h>
+
+
+#define PORT (in_port_t) 50009
+
+
+
+
+
+
+#define BUF_LENGTH 1024
+#define HOSTNAME_LENGTH 64
+#define FILENAME_LENGTH 128
